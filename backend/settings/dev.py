6441dab28fcc80c2b946e36fb36abb308b878115
Jeffrey 'Alex' Clark <aclark@aclark.net>
2024-08-20 15:42:05 -0400

    Update project-makefile

--- backend/settings/dev.py
+++ backend/settings/dev.py
@@ -50,4 +50,4 @@ INTERNAL_IPS = [
 MIDDLEWARE.append("debug_toolbar.middleware.DebugToolbarMiddleware")  # noqa
 MIDDLEWARE.append("hijack.middleware.HijackUserMiddleware")  # noqa
 INSTALLED_APPS.append("django.contrib.admindocs")  # noqa
-SECRET_KEY = "YPhnPuxDelcZ5sCbsZrN7npsNB8jaGvq2qJCcjvXdD9XZmG/gaeGPmMoH0bPPDBU"
+SECRET_KEY = "HLlPf1JKCX5w4QZqzWGBlMy2V3VBJTWrAF32FGx1RPRmni8h/5czsFp1/eqkpejm"
